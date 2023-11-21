b83839afbf41f23b7ae06f9dcaef32d8f374653d
Pierre Carrier <pc@rrier.fr>
2023-11-21 12:42:18 +0100

    loading state

--- src/admin.tsx
+++ src/admin.tsx
@@ -1,22 +1,38 @@
 import { Header } from "./header.tsx";
-import { StateCtx } from "./app.tsx";
+import { State, StateCtx } from "./app.tsx";
 import { route } from "preact-router";
 import { useContext } from "preact/hooks";
+
+function AdminBody({ state }: { state: State }) {
+  return (
+    <>
+      User #{state.kv.get("session").get(1)}. Admin interface incoming 😅 Check
+      out our <a href="https://demo.xmit.co/landed.html">prototype</a>.
+    </>
+  );
+}
 
 export function Admin() {
   const state = useContext(StateCtx);
   const session = state.value.kv.get("session");
+  let lockedAndLoaded = true;
   if (session === undefined || session.get(1) === undefined) {
-    route("/");
-    return <></>;
+    if (state.value.ready) {
+      route("/");
+    } else {
+      lockedAndLoaded = false;
+    }
   }
 
   return (
     <div class="with-header">
       <Header />
       <div class="body">
-        User #{session.get(1)}. Admin interface incoming 😅 Check out our{" "}
-        <a href="https://demo.xmit.co/landed.html">prototype</a>.
+        {lockedAndLoaded ? (
+          <AdminBody state={state.value} />
+        ) : (
+          <em>Loading…</em>
+        )}
       </div>
     </div>
   );
