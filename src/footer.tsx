e377207dffe5401d48680e95a67e4ed0b0c77e44
Pierre Carrier <pc@rrier.fr>
2024-04-08 17:22:10 +0200

    un-duh

--- src/footer.tsx
+++ src/footer.tsx
@@ -5,10 +5,6 @@ export function Footer() {
         Free while we can. <a href="mailto:sponsorship@xmit.dev">Sponsor us!</a>
       </p>
       <p>
-        <a href="https://pcarrier.com">pcarrier.ca Software Inc.</a> covers this
-        week. Happy to help.
-      </p>
-      <p>
         Check out our <a href="https://xmit.dev">dev blog</a>.
       </p>
     </footer>
