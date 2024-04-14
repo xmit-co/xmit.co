c9be8cb2edb2f1f63ed8f59992757d95798574ec
Pierre Carrier <pc@rrier.fr>
2024-04-14 11:17:13 +0200

    suppor@xmit.dev

--- src/footer.tsx
+++ src/footer.tsx
@@ -5,7 +5,8 @@ export function Footer() {
         Free while we can. <a href="mailto:sponsorship@xmit.dev">Sponsor us!</a>
       </p>
       <p>
-        Check out our <a href="https://xmit.dev">dev blog</a>.
+        Check out our <a href="https://xmit.dev">dev blog</a>,{" "}
+        <a href="mailto:help@xmit.dev">reach out</a> for help and suggestions.
       </p>
     </footer>
   );
